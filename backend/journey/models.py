e29d9f5ae0cb0be70c36f95f4b57263912433598
Nitish Kumar Giri <girinitishkumar11@gmail.com>
2026-09-16 01:27:15 +0530

    Created the Journey model to store customer Journey from taking an insurance.

--- backend/journey/models.py
+++ backend/journey/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Journey(models.Model):
+    patient = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    active_policy =	models.ForeignKey("insurance.Policy", on_delete=models.SET_NULL, null=True)
+    current_stage = models.CharField(max_length=30,	default="admission")
+
+
+class JourneyStageLog(models.Model):
+    journey = models.ForeignKey(Journey, related_name="stage_history",	on_delete=models.CASCADE)
+    stage = models.CharField(max_length=30)
+    timestamp = models.DateTimeField(auto_now_add=True)
