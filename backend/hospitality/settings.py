b0f406d6092cfa66662a2a2823cd8680776e2729
Nitish Kumar Giri <girinitishkumar11@gmail.com>
2026-09-15 14:45:12 +0530

    Updated Settings.py by adding all apps and malware.

--- backend/hospitality/settings.py
+++ backend/hospitality/settings.py
@@ -37,9 +37,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "rest_framework",
+    "corsheaders",
+
+    "users",
+    "insurance",
+    "hospitals",
+    "journey",
+    "ai_explainer",
 ]
 
 MIDDLEWARE = [
+     "corsheaders.middleware.CorsMiddleware",
+
+
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'hospitality.urls'
 
