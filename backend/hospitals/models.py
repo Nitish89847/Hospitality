cf5693cc6f58e04bfa5ea201264aa064b8a15c1f
Nitish Kumar Giri <girinitishkumar11@gmail.com>
2026-09-17 14:18:42 +0530

    Accepted Null values in empanelled_schemes and added validation for No null value combined.

--- backend/hospitals/models.py
+++ backend/hospitals/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -8,8 +9,15 @@ class Hospital(models.Model):
 	latitude = models.FloatField(null=True,	blank=True)
 	longitude = models.FloatField(null=True, blank=True)
 	specialties	= models.JSONField(default=list)          #cardiology neurology orthopedics pediatrics obstetrics gynecology oncology ophthalmology dermatology psychiatry ENT gastroenterology nephrology pulmonology rheumatology endocrinology urology
-	empanelled_schemes = models.JSONField(default=list)			#	["PMJAY",	"ESI"]
+	empanelled_schemes = models.JSONField(default=list,null=True, blank=True)			#	["PMJAY",	"ESI"]
 	network_insurers = models.JSONField(default=list,null=True, blank=True)					#	insurer	names	this	hospital	accepts
+
+
+	def clean(self):
+		if not self.empanelled_schemes and not self.network_insurers:
+			raise ValidationError(
+				"A hospital must have at least one empanelled scheme or one network insurer."
+			)
 
 class RoomType(models.Model):
 	hospital = models.ForeignKey(Hospital, related_name="room_types", on_delete=models.CASCADE)
