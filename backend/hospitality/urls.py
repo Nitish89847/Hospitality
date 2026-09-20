2eccbfe045432e34f279249bbee13f5f7258c3c3
Nitish Kumar Giri <girinitishkumar11@gmail.com>
2026-09-20 10:59:36 +0530

    Added JWT authentication for API access.

--- backend/hospitality/urls.py
+++ backend/hospitality/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/insurance/", include("insurance.urls")),
     path("api/hospitals/", include("hospitals.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
